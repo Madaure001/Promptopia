f51d00893cfc9cd6b7fc2c51c10deda80ac6a486
Madaure Thulani <calnganyi@gmail.com>
2024-04-09 10:38:19 +0200

    useSearch typo fixed

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <section className="w-full flex-center flex-col">
         {/* heading and sub-heading */}
         <h1 className="head_text text-center">
-            Discover &Share
+            Discover & Share
             <br className="max-md:hidden" />
             <span className="orange_gradient text-center">AI-Powered Prompts</span>
         </h1>
